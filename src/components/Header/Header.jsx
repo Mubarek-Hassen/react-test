15c357053e0daebd6841216b8e93b58f7613330b
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-12-15 23:56:18 -0500

    Setup and utilize material ui

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+import ResponsiveDrawer from "./ResponsiveDrawer";
+import { Container } from "@mui/material";
+const Header =()=>{
+  return(
+    <Container>
+      <ResponsiveDrawer />
+    </Container>
+  )
+}
+
+export default Header;
